df82b09021710c65a7f7214bc5cbe6133bc5fd8c
justinjmueller <justin.mueller@colostate.edu>
2023-11-17 16:18:09 -0600

    Fix formatting

--- include/definitions.h
+++ include/definitions.h
@@ -12,39 +12,39 @@
 #include "sbnana/CAFAna/Core/Var.h"
 #include "sbnana/CAFAna/Core/Cut.h"
 
-#define VARDLP_RECO(NAME,VAR,SEL)                           \
-  const SpillMultiVar NAME([](const caf::SRSpillProxy* sr)  \
-  {                                                         \
-    std::vector<double> var;                                \
-    for(auto const& i : sr->dlp)                            \
-    {                                                       \
-	    if(SEL(i))                                          \
-	        var.push_back(VAR(i));                          \
-    }                                                       \
-    return var;                                             \
-  })
+#define VARDLP_RECO(NAME,VAR,SEL)                             \
+    const SpillMultiVar NAME([](const caf::SRSpillProxy* sr)  \
+    {                                                         \
+        std::vector<double> var;                              \
+        for(auto const& i : sr->dlp)                          \
+        {                                                     \
+            if(SEL(i))                                        \
+                var.push_back(VAR(i));                        \
+        }                                                     \
+        return var;                                           \
+    })
 
-#define VARDLP_TRUE(NAME,VAR,SEL)                           \
-  const SpillMultiVar NAME([](const caf::SRSpillProxy* sr)  \
-  {                                                         \
-    std::vector<double> var;                                \
-    for(auto const& i : sr->dlp_true)                       \
-    {                                                       \
-	    if(SEL(i))                                          \
-	        var.push_back(VAR(i));                          \
-    }                                                       \
-    return var;                                             \
-  })
+#define VARDLP_TRUE(NAME,VAR,SEL)                             \
+    const SpillMultiVar NAME([](const caf::SRSpillProxy* sr)  \
+    {                                                         \
+        std::vector<double> var;                              \
+        for(auto const& i : sr->dlp_true)                     \
+        {                                                     \
+            if(SEL(i))                                        \
+                var.push_back(VAR(i));                        \
+        }                                                     \
+        return var;                                           \
+    })
 
-#define VARDLP_TTP(NAME,VAR,CAT,SEL)                               \
-  const SpillMultiVar NAME([](const caf::SRSpillProxy* sr)         \
-  {                                                                \
-    std::vector<double> var;                                       \
-    for(auto const& i : sr->dlp_true)                              \
-    {                                                              \
-      if(CAT(i) && i.match.size() > 0 && SEL(sr->dlp[i.match[0]])) \
-	        var.push_back(VAR(sr->dlp[i.match[0]]));               \
-    }                                                              \
-    return var;                                                    \
-  })
+#define VARDLP_TTP(NAME,VAR,CAT,SEL)                                     \
+    const SpillMultiVar NAME([](const caf::SRSpillProxy* sr)             \
+    {                                                                    \
+        std::vector<double> var;                                         \
+        for(auto const& i : sr->dlp_true)                                \
+        {                                                                \
+            if(CAT(i) && i.match.size() > 0 && SEL(sr->dlp[i.match[0]])) \
+            var.push_back(VAR(sr->dlp[i.match[0]]));                     \
+        }                                                                \
+        return var;                                                      \
+    })
 #endif
