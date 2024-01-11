c061828c0640e82a409e8c68b929acbd0de5e108
justinjmueller <justin.mueller@colostate.edu>
2024-01-11 10:02:18 -0600

    Changed make_plots.py to accept a subdirectory as an argument

--- plotting/make_plots.py
+++ plotting/make_plots.py
@@ -1,14 +1,16 @@
 import yaml
 import uproot
+import sys
 from plot_funcs import PlotDescription, plot_histogram_1d, plot_histogram_2d, plot_confusion, plot_flow
 
-def main():
+def main(args):
   cfg_file = open('plot_configurations.yml', 'r')
   cfg = yaml.safe_load(cfg_file)
   rf = uproot.open('../spectra.root')
 
   for h in cfg:
     desc = PlotDescription(h)
+    desc.save = f'plots/{args[0] + "/" if args[0] != "" else ""}{desc.save}'
     if desc.type == 'hist1d':
       plot_histogram_1d(rf, desc)
     elif desc.type == 'hist2d':
@@ -19,4 +21,7 @@ def main():
       plot_flow(rf, desc)
 
 if __name__ == '__main__':
-    main()
+  if len(sys.argv) == 2:
+    main(sys.argv[1:])
+  else:
+    main(['',])
