ea39951d0a8a6e4e23e1fdff1886de5dcb22d250
justinjmueller <justin.mueller@colostate.edu>
2024-01-11 08:26:16 -0600

    Added some comments to make variables more clear

--- include/analysis.h
+++ include/analysis.h
@@ -12,21 +12,23 @@ using namespace ana;
 
 namespace ana
 {
+    // "Simple" variables that attach to a single interaction attribute.
     VARDLP_RECO(kCountParticles,vars::count_particles,cuts::no_cut);
     VARDLP_RECO(kCountPrimaries,vars::count_primaries,cuts::no_cut);
 
     VARDLP_TRUE(kCountParticlesTruth,vars::count_particles,cuts::no_cut);
     VARDLP_TRUE(kCountPrimariesTruth,vars::count_primaries,cuts::no_cut);
 
+    // Define "variables" for binning interactions by some categorical class.
     DEFINECAT();
+
+    // Define variables that are broadcasted across each selection cut.
     TCATVAR(kCountTTP,count);
     RCATVAR(kCountPTT,count);
     TCATVAR(kVisibleEnergyTTP,visible_energy);
     RCATVAR(kFlashTime,flash_time);
-
-    PVARDLP_RECO(kCSDA,vars::csda_ke,cuts::no_cut);
-    PVARDLP_TRUE(kCSDATruth,vars::csda_ke,cuts::no_cut,cuts::no_cut);
     
+    // Variables for confusion matrix.
     PVARDLP_TRUE(kPrimaryTruth,vars::primary,cuts::no_cut,cuts::matched);
     PVARDLP_TRUE(kPIDTruth,vars::pid,cuts::no_cut,cuts::matched);
     PVARDLP_TRUE(kPrimaryPIDTruth,vars::primary_pid,cuts::no_cut,cuts::matched);
@@ -34,14 +36,15 @@ namespace ana
     PVAR_TTP(kPID,vars::pid,cuts::no_cut,cuts::no_cut,cuts::no_cut);
     PVAR_TTP(kPrimaryPID,vars::primary_pid,cuts::no_cut,cuts::no_cut,cuts::no_cut);
 
+    // Variables for neutrino-only confusion matrix.
     PVARDLP_TRUE(kPrimaryTruth_Neutrino,vars::primary,cuts::neutrino,cuts::matched);
     PVARDLP_TRUE(kPIDTruth_Neutrino,vars::pid,cuts::neutrino,cuts::matched);
     PVARDLP_TRUE(kPrimaryPIDTruth_Neutrino,vars::primary_pid,cuts::neutrino,cuts::matched);
-    // "Primary ID for reco particles in interactions passing the neutrino cut"
     PVAR_TTP(kPrimary_Neutrino,vars::primary,cuts::neutrino,cuts::no_cut,cuts::no_cut);
     PVAR_TTP(kPID_Neutrino,vars::pid,cuts::neutrino,cuts::no_cut,cuts::no_cut);
     PVAR_TTP(kPrimaryPID_Neutrino,vars::primary_pid,cuts::neutrino,cuts::no_cut,cuts::no_cut);
 
+    // Variables for cosmic-only confusion matrix.
     PVARDLP_TRUE(kPrimaryTruth_Cosmic,vars::primary,cuts::cosmic,cuts::matched);
     PVARDLP_TRUE(kPIDTruth_Cosmic,vars::pid,cuts::cosmic,cuts::matched);
     PVARDLP_TRUE(kPrimaryPIDTruth_Cosmic,vars::primary_pid,cuts::cosmic,cuts::matched);
@@ -49,8 +52,10 @@ namespace ana
     PVAR_TTP(kPID_Cosmic,vars::pid,cuts::cosmic,cuts::no_cut,cuts::no_cut);
     PVAR_TTP(kPrimaryPID_Cosmic,vars::primary_pid,cuts::cosmic,cuts::no_cut,cuts::no_cut);
 
-    // "Loop over true particles within true interactions and return X for all true particles with a reco particle match"
+    // Variables for 2D "true vs. reco" style plots.
+    PVARDLP_TRUE(kCSDATruth,vars::csda_ke,cuts::no_cut,cuts::no_cut);
+    PVAR_TTP(kCSDA,vars::csda_ke,cuts::no_cut,cuts::no_cut,cuts::no_cut);
+
     PVARDLP_TRUE(kLowXTruth,vars::lowx,cuts::no_cut,cuts::matched);
-    // "Loop over true particles within true interactions and return X for all reco particles that are matched to by true particles"
     PVAR_TTP(kLowX,vars::lowx,cuts::no_cut,cuts::no_cut,cuts::no_cut);
 }
