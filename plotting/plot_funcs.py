6221b90db7756769ba54e5b1b14efa76d178f608
justinjmueller <justin.mueller@colostate.edu>
2024-01-16 10:02:02 -0600

    Fixed bug that capped 2D histogram color axis at 1

--- plotting/plot_funcs.py
+++ plotting/plot_funcs.py
@@ -188,7 +188,7 @@ def plot_histogram_2d(rf, desc):
 
     contents, xedges, yedges = load_histograms(rf, desc.var)
     x, y = np.meshgrid(xedges, yedges)
-    pc = ax.pcolormesh(x, y, contents, cmap='Blues', vmin=0, vmax=1.0)
+    pc = ax.pcolormesh(x, y, contents, cmap='Blues')
 
     ax.set_xlabel(desc.xlabel)
     ax.set_ylabel(desc.ylabel)
