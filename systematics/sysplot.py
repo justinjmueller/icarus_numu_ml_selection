763cfad103eef82edd01d75c8d73b5d77604a9ac
justinjmueller <justin.mueller@colostate.edu>
2024-03-06 15:51:15 -0600

    Refactor systematic plotting code

--- systematics/sysplot.py
+++ systematics/sysplot.py
@@ -2,6 +2,8 @@ import numpy as np
 import pandas as pd
 import awkward as ak
 import matplotlib.pyplot as plt
+from matplotlib.collections import PatchCollection
+from matplotlib.patches import Rectangle, Patch
 import argparse
 import uproot
 import toml
@@ -29,7 +31,35 @@ class PlotDescription:
         for k, v in cfg.items():
             setattr(self, k, v)
 
-def plot_histogram_with_systematics(desc, selected, cov_path):
+def add_error_boxes(ax, x, y, xerr, yerr, **kwargs):
+    """
+    Adds error boxes to the input axis.
+
+    Parameters
+    ----------
+    ax: matplotlib.axes.Axes
+        The axis to which the error boxes are to be added.
+    x: numpy.array
+        The x-coordinates of the error boxes.
+    y: numpy.array
+        The y-coordinates of the error boxes.
+    xerr: numpy.array
+        The x-error values of the error boxes.
+    yerr: numpy.array
+        The y-error values of the error boxes.
+    kwargs: dict
+        Keyword arguments to be passed to the errorbar function.
+
+    Returns
+    -------
+    None.
+    """
+    boxes = [Rectangle((x[i] - xerr[i], y[i] - yerr[i]), 2 * np.abs(xerr[i]), 2 * yerr[i]) for i in range(len(x))]
+    pc = PatchCollection(boxes, **kwargs)
+    ax.add_collection(pc)
+    return boxes[0]
+
+def plot_histogram(desc, selected, cov):
     """
     Plots a 1D histogram with the specified plot configurations for the
     input selected candidates.
@@ -43,69 +73,139 @@ def plot_histogram_with_systematics(desc, selected, cov_path):
         A DataFrame containing basic information about the selected
         neutrinos (run, subrun, event, nu_id) along with any binned
         variables (e.g. reconstructed energy).
-    cov_path: str
+    cov: str
         The full path to the numpy zip file containing the covariance
         matrix.
 
     Returns
     -------
-    None.
+    figure: matplotlib.figure.Figure
+        The figure object containing the histogram plot.
     """
-    figure = plt.figure(figsize=(8,6))
-    ax = figure.add_subplot()
+    if desc.multiplot != 'none':
+        figure = plt.figure(figsize=(8,8))
+        gspec = figure.add_gridspec(2, 1, height_ratios=[4, 1])
+        rax = figure.add_subplot(gspec[1])
+        ax = figure.add_subplot(gspec[0], sharex=rax)
+    else:
+        figure = plt.figure(figsize=(8,6))
+        ax = figure.add_subplot()
 
     contents = list()
     centers = list()
     labels = list()
+    width = list()
     for m in desc.merge[::-1]:
         mask = np.isin(selected[desc.categorical_var], m)
         c, e = np.histogram(selected[desc.var][mask], bins=desc.bins[0], range=desc.bins[1:])
         contents.append(c)
         centers.append((e[1:] + e[:-1]) / 2.0)
+        width.append(np.diff(e))
         labels.append(desc.categories[m[0]])
-
-    covariance = np.load(cov_path)
-    error = np.sqrt(np.diagonal(covariance[desc.sys_var]))
 
     colors = [f'C{i}' for i in desc.colors][::-1]
     ax.hist(centers, weights=contents, bins=desc.bins[0], range=desc.bins[1:], label=labels, color=colors, histtype='barstacked')
-    ax.errorbar(centers[0], np.sum(contents, axis=0), yerr=error, fmt='o', c='black')
-    ax.set_xlim(*desc.bins[1:])
-    ax.set_ylim(*desc.ylim)
     h, l = ax.get_legend_handles_labels()
-    ax.legend(h[::-1], l[::-1])
-    ax.set_xlabel(desc.xlabel)
-    ax.set_ylabel('Entries')
-    figure.suptitle(f'Selected 1$\mu$1p Candidates\nError Source: {desc.sys_var.capitalize()}')
-    figure.savefig(desc.save)
-    plt.close(figure)
+    show_percentage = hasattr(desc, 'show_percentage') and desc.show_percentage
+    if show_percentage:
+        l = [f'{l} ({np.sum(contents[li]):.0f}, {np.sum(contents[li]) / np.sum(contents):.02%})'for li, l in enumerate(l)]
+    else:
+        l = [f'{l} ({np.sum(contents[li]):.0f})'for li, l in enumerate(l)]
+    h = h[::-1]
+    l = l[::-1]
 
-def main(configuration, plot, log, cov, output):
-    header = [  'run', 'subrun', 'event', 'nu_id', 'category', 'category_topology', 'category_mode',
-                'visible_energy', 'leading_muon_ke', 'leading_proton_ke', 'leading_muon_pt',
-                'leading_proton_pt', 'interaction_pt']
-    selected = read_log(log, 'SELECTED_1MU1P', header)
-
-    cfg = dict()
-    if isinstance(configuration[0], list):
-        configuration = [c for y in configuration for c in y]
-    for c in configuration:
-        with open(c, 'r') as f:
-            cfg = {**cfg, **toml.load(f)}
-    plot_keys = list(cfg.keys()) if plot is None else plot
+    covariance = np.load(cov)
+    ccycle = iter(['gray', 'rebeccapurple'])
+    hcycle = iter(['///', ''])
+    for syskey, sysname in desc.systematics.items():
+        error = np.sqrt(np.diagonal(covariance[f'{syskey}_{desc.var}']))
+        c = next(ccycle)
+        a = next(hcycle)
+        add_error_boxes(ax, centers[0], np.sum(contents, axis=0), width[0] / 2, error, facecolor=c, edgecolor='none', alpha=0.5, hatch=a)
+        h.append(Patch(facecolor=c, edgecolor=c, alpha=0.5, hatch=a))
+        l.append(sysname)
     
-    pbar = tqdm(plot_keys)
-    for k in pbar:
-        pbar.set_description(f'Making plot "{k}"')
-        desc = PlotDescription(k, cfg[k], output)
-        plot_histogram_with_systematics(desc, selected, f'{cov}_{desc.var}.npz')
+    ax.legend(h, l)
+
+    if desc.multiplot == 'ratio':
+        vnominal = covariance[f'{syskey}_{desc.var}_vnominal']
+        singular_mask = vnominal != 0
+        chi2 = vnominal[singular_mask] @ np.linalg.inv(covariance[f'{syskey}_{desc.var}_rmatrix'][singular_mask,:][:,singular_mask]) @ vnominal[singular_mask].transpose()
+        #print(chi2, 1-stats.chi2.cdf(chi2, np.sum(singular_mask)))
+
+        sys = list(desc.systematics.items())
+        ratio = covariance[f'{sys[0][0]}_{desc.var}_ratio']
+        undefined = (ratio == 1)
+        error = np.sqrt(np.diagonal(covariance[f'{sys[0][0]}_{desc.var}_cratio']))
+        rax.errorbar(x=centers[0][~undefined], y=ratio[~undefined], yerr=error[~undefined], c='black', fmt='o')
+        rax.axhline(1, c='black', ls='--')
+        rax.set_ylim(0.8, 1.2)
+        rax.set_xlim(*desc.bins[1:])
+        rax.set_xlabel(desc.xlabel)
+        rax.set_ylabel('Sys. / CV')
+        plt.setp(ax.get_xticklabels(), visible=False)
+    elif desc.multiplot == 'error':
+        error = np.sqrt(np.diagonal(covariance[f'{syskey}_{desc.var}']))
+        total = np.sum(contents, axis=0)
+        rax.errorbar(centers[0], 100*np.divide(error, total, where=total!=0), xerr=width[0] / 2, yerr=0, fmt='_', c='black')
+        rax.set_xlim(*desc.bins[1:])
+        rax.set_xlabel(desc.xlabel)
+        rax.set_ylim(0, 50)
+        rax.set_ylabel('Rel. Error [%]')
+        plt.setp(ax.get_xticklabels(), visible=False)
+    else:
+        ax.set_xlim(*desc.bins[1:])
+        ax.set_xlabel(desc.xlabel)
+
+    ax.set_ylim(*desc.ylim)
+    ax.set_ylabel(desc.ylabel)
+    ax.set_title(desc.title)
+    figure.savefig(desc.save)
+
+    return figure
+
+def main(configuration, plots, log, cov_path, output):
+    """
+    Main function for making systematic-related plots.
+
+    Parameters
+    ----------
+    configuration: str
+        The full path to the TOML configuration file containing the
+        plot settings.
+    plots: list[str]
+        The list of the plots to be made. If None, all plots in the
+        configuration file are made.
+    log: str
+        The full path to the log file containing the selected events.
+    cov_path: str
+        The full path to the numpy zip file containing the covariance
+        matrix.
+    output: str
+        The full path specifying the save location for the plot.
+    """
+    # Read the configuration file and extract the plot settings
+    cfg = toml.load(configuration)
+    header = cfg['metadata']['columns'] + list(cfg['variables'].keys())
+    all = {f'{plot_name}_{var_name}': dict(**plot_cfg, **var_cfg) for plot_name, plot_cfg in cfg['plots'].items() for var_name, var_cfg in cfg['variables'].items()}
+    if plots != None:
+        all = {k: v for k, v in all.items() if k in plots}
+
+    # Loop over the configured plots and make them
+    pbar = tqdm(all.items())
+    for k, v in pbar:
+        pbar.set_description(f'{"Making plot `" + k + "`":^45}')
+        selected = read_log(log, f'SELECTED_{v["channel"].upper()}', header)
+        cov = f'{cov_path}covariances_{v["channel"]}.npz'
+        v['ylim'] = v['ylim'][v['channel']]
+        plot_histogram(PlotDescription(k, v, output), selected, cov)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-l', '--log', default='../output.log')
     parser.add_argument('-e', '--covariance')
-    parser.add_argument('-c', '--config', action='append', nargs='*')
+    parser.add_argument('-c', '--config')
     parser.add_argument('-p', '--plot', action='append')
-    parser.add_argument('-o', '--output')
+    parser.add_argument('-o', '--output', default='./')
     args = parser.parse_args()
     main(args.config, args.plot, args.log, args.covariance, args.output)
